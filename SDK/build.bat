8146df6c2f472357589b6248d286858d9dc019f2
gforest_shade <shinapoka@gmail.com>
2017-05-21 12:38:18 +0900

    update build.bat

--- SDK/build.bat
+++ SDK/build.bat
@@ -1,6 +1,15 @@
+@echo off
+
 rem VS2015�̏ꍇ
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 
 nmake Final_Release
+if not "%ERRORLEVEL%"  == "0" (
+   echo "compile failed."
+   pause
+   exit
+)
 copy /Y Final_Release\CvGameCoreDLL.dll ..\Assets
+
 pause
+
