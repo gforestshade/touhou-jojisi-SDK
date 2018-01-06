621bdb757965dcd8a7dfe66bb74d27ecba52bed8
gforest_shade <shinapoka@gmail.com>
2018-01-07 03:19:15 +0900

    VS2017 with win10 に移行

--- SDK/build.bat
+++ SDK/build.bat
@@ -1,5 +1,6 @@
 rem VS2015�̏ꍇ
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+cd %~dp0
 
 nmake Final_Release
 copy /Y Final_Release\CvGameCoreDLL.dll ..\Assets
